de41a669964cb81e5b1ac190ec6c8d6573b7ce3c
David Fischer <fiscd@seas.upenn.edu>
2025-03-11 09:58:21 -0400

    commit

--- app/components/FaviconButton.tsx
+++ app/components/FaviconButton.tsx
@@ -1,21 +1,22 @@
 // components/FaviconButton.tsx
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
 
 export default function FaviconButton() {
+  const reloadPage = () => {
+    window.location.href = "/"; // This forces a full page reload
+  };
+
   return (
-    <Link href="/" passHref>
-      <button className="favicon-button">
-        <Image
-          src="/favicon.ico"
-          alt="Home"
-          width={40}
-          height={40}
-          className="favicon"
-        />
-      </button>
-    </Link>
+    <button className="favicon-button" onClick={reloadPage}>
+      <Image
+        src="/favicon.ico"
+        alt="Home"
+        width={40}
+        height={40}
+        className="favicon"
+      />
+    </button>
   );
 }
